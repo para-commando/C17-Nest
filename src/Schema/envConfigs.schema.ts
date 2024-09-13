b17a9a0588bba9523300b869ea9e46d8718c3353
para-commando <anirudh.nayak.172k@gmail.com>
2024-09-13 17:05:46 +0530

    dynamic port and host using config values

--- src/Schema/envConfigs.schema.ts
+++ src/Schema/envConfigs.schema.ts
@@ -8,4 +8,5 @@ export const envValidationSchema = Joi.object({
   DB_PASSWORD: Joi.string().required(),
   DB_NAME: Joi.string().required(),
   DB_USER: Joi.string().required(),
+  APP_PORT: Joi.number().port().required(),
 });
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import { ConfigService } from '@nestjs/config';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const config = new ConfigService();
+  const APP_PORT =config.get<number>('APP_PORT') || 3000;
+  const APP_HOST =config.get<number>('APP_HOST') || 'localhost';
+  app.enableCors();
+  await app.listen(APP_PORT);
+  console.log(`🪖  Ghatak-Nest 🪺  can be accessed on 📡 http://${APP_HOST}:${APP_PORT} 🛰️`);
 }
 bootstrap();
