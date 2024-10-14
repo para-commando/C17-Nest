4c791873b0a89b558fa6eb6c9cb9eae8b66f1b6b
para-commando <anirudh.nayak.172k@gmail.com>
2024-10-14 21:43:00 +0530

    removing some redundant files

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,7 +7,6 @@ import {
 } from './shared/config/databaseConfig';
 import { AppService } from './app.service';
 import { AppController } from './app.controller';
-import { UserModule } from './user/user.module';
 import { CacheModule } from './shared/cache/cache.module';
 import loadYamlConfig from './shared/config/loadYamlConfig';
 import { ServeStaticModule } from '@nestjs/serve-static';
@@ -15,7 +14,6 @@ import { join } from 'path';
 import { LogDefinitionService } from './shared/log-definition/log-definition.service';
 import { LogDefinitionMiddleware } from './shared/log-definition/log-definition.middleware';
 import { OrdersModule } from './api/orders/orders.module';
-import { ClientsModule } from './api/clients/clients.module';
 import { DeliveriesModule } from './api/deliveries/deliveries.module';
 import { InventoriesModule } from './api/inventories/inventories.module';
 import { NotificationsModule } from './api/notifications/notifications.module';
@@ -32,7 +30,6 @@ import { ClientsModule } from './api/clients/clients.module';
       envFilePath: ['.env.local', '.env'],
       isGlobal: true,
     }),
-    UserModule,
     CacheModule,
     OrdersModule,
     ClientsModule,
